61aeadc8acc9393c182b9f5315790bdfffc8123e
Tobias Bodor <ext_tobias.bodor@cluno.com>
2020-04-29 17:49:08 +0200

    Change component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .hello {
-  background: white;
+  background: green;
 }
 h3 {
   margin: 40px 0 0;
