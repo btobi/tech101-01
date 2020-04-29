9c3ac75a5e99af373a7a00375141b7d2de572d92
Tobias Bodor <ext_tobias.bodor@cluno.com>
2020-04-29 17:25:04 +0200

    Change component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Tech101" />
   </div>
 </template>
 
@@ -16,7 +15,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
